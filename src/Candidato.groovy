6355ff5f21c94e162a235b36e81347f76b35383f
Thiago Nascimento <thiagosilvanascimento2015@gmail.com>
2022-08-29 12:06:44 -0300

    first commit

---
+++ src/Candidato.groovy
@@ -0,0 +1,11 @@
+class Candidato {
+    String nome
+    String email
+    String cpf
+    int idade
+    String estado
+    String cep
+    String descricaoPessoal
+    List<String> competencias
+}
+
---
+++ src/Empresa.groovy
@@ -0,0 +1,10 @@
+class Empresa {
+    String nome
+    String emailCorporativo
+    String cnpj
+    String pais
+    String estado
+    String cep
+    String descricaoEmpresa
+    List<String> competencias
+}
